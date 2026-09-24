5da9295e553850a0af4e821293970b47e3270c0b
syedmahin272-cloud <syedmahin272@gmail.com>
2026-09-24 12:14:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import time
 from flask import Flask, request
 import os
 
-# Bot Config (Notun Token dewa hoyeche)
+# Bot Config
 BOT_TOKEN = "8668990603:AAHMkDqp_NwpuhVRrFnI6qYHIr2HoiB2NuE"
 ADMIN_ID = 7266067201
 WEBHOOK_URL = "https://grizzlysms-8ex5.onrender.com"
@@ -18,7 +18,7 @@ app = Flask(__name__)
 maintenance_mode = False
 user_api_keys = {}
 
-# Checker API Config
+# Checker API Config[cite: 1]
 CHECKER_URL = "http://api.agbots.site:8080/check/"
 CHECKER_AUTH = "user8354"
 CHECKER_API_KEY = "SIGUzg7Xf7euGs8B"
@@ -36,17 +36,17 @@ def check_tg_number(phone_number):
         phone_number = "+" + phone_number
         
     payload = {
-        "auth": CHECKER_AUTH,
-        "api_key": CHECKER_API_KEY,
-        "phone_numbers": [phone_number]
+        "auth": CHECKER_AUTH,[cite: 1]
+        "api_key": CHECKER_API_KEY,[cite: 1]
+        "phone_numbers": [phone_number][cite: 1]
     }
     
     try:
-        response = requests.get(CHECKER_URL, json=payload, timeout=60)
-        if response.status_code == 200:
-            data = response.json()
-            if str(data.get("status")) == "200":
-                result_obj = data.get("result_obj", {})
+        response = requests.get(CHECKER_URL, json=payload, timeout=60)[cite: 1]
+        if response.status_code == 200:[cite: 1]
+            data = response.json()[cite: 1]
+            if str(data.get("status")) == "200":[cite: 1]
+                result_obj = data.get("result_obj", {})[cite: 1]
                 status = str(result_obj.get(phone_number, "unknown")).lower()
                 return STATUS_EMOJIS.get(status, "❓")
     except Exception:
@@ -66,10 +66,10 @@ def wait_for_otp(chat_id, api_key, activation_id, phone_number):
             if response_text.startswith("STATUS_OK"):
                 otp = response_text.split(":")[1]
                 
-                text = f"🇨🇴 Telegram {phone_number}"
+                text = f"🇨🇴 Telegram {phone_number}"[cite: 2]
                 
                 markup = InlineKeyboardMarkup()
-                markup.add(InlineKeyboardButton(text=f"{otp}", copy_text=CopyTextButton(text=otp)))
+                markup.add(InlineKeyboardButton(text=f"{otp}", copy_text=CopyTextButton(text=otp)))[cite: 2]
                 
                 bot.send_message(chat_id, text, reply_markup=markup)
                 return
@@ -170,7 +170,6 @@ if __name__ == "__main__":
     bot.remove_webhook()
     time.sleep(1)
     
-    # drop_pending_updates add kora hoyeche jate stuck howa msg clear hoye jay
     bot.set_webhook(url=f"{WEBHOOK_URL}/{BOT_TOKEN}", drop_pending_updates=True)
     print(f"Webhook set successfully to {WEBHOOK_URL}")
     
