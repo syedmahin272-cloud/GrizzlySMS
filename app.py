44877cc9a3b4b466a584e701f8063cd6a6c53888
syedmahin272-cloud <syedmahin272@gmail.com>
2026-09-24 22:42:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask
 import os
 
 # Bot Config
-BOT_TOKEN = "8668990603:AAHMkDqp_NwpuhVRrFnI6qYHIr2HoiB2NuE"
+BOT_TOKEN = "8867616150:AAFZj74WqvXyspMLmIWrZnZlzl9g_1TwqW4"
 ADMIN_ID = 7266067201
 
 bot = telebot.TeleBot(BOT_TOKEN)
